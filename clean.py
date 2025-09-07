538a9b2cbbb566c412571c4eb29385943ae5ea8a
harishkannarao <harishkannarao@users.noreply.github.com>
2025-09-07 09:18:57 +0100

    Fixing supplementary script

--- clean.py
+++ clean.py
@@ -1,7 +1,13 @@
 import os
 
-if os.path.exists("requirements.txt"):
-    os.remove("requirements.txt")
 
-if os.path.exists("python-exercise.zip"):
-    os.remove("python-exercise.zip")
+def main():
+    if os.path.exists("requirements.txt"):
+        os.remove("requirements.txt")
+
+    if os.path.exists("python-exercise.zip"):
+        os.remove("python-exercise.zip")
+
+
+if __name__ == '__main__':
+    main()
--- create_distribution.py
+++ create_distribution.py
@@ -5,15 +5,20 @@ import os
 def zip_files_in_dir(dir_name, zip_obj):
     for folder, sub_folder, filenames in os.walk(dir_name):
         for filename in filenames:
-            zip_obj.write(os.path.join(folder, filename),
-                          os.path.relpath(os.path.join(folder, filename), os.path.join(dir_name, '..')))
+            file_name = str(os.path.join(folder, filename))
+            zip_obj.write(file_name, os.path.relpath(file_name, os.path.join(dir_name, '..')))
 
 
 def zip_file(file_name, zip_obj):
     zip_obj.write(file_name)
 
 
-with ZipFile('python-exercise.zip', 'w', ZIP_DEFLATED) as zipObj:
-    zip_file('main.py', zipObj)
-    zip_file('requirements.txt', zipObj)
-    zip_files_in_dir('my_module', zipObj)
+def main():
+    with ZipFile('python-exercise.zip', 'w', ZIP_DEFLATED) as zipObj:
+        zip_file('main.py', zipObj)
+        zip_file('requirements.txt', zipObj)
+        zip_files_in_dir('my_module', zipObj)
+
+
+if __name__ == '__main__':
+    main()
