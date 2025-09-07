e64983483314dae94a39509621c4dbfbaf4f4512
harishkannarao <harishkannarao@users.noreply.github.com>
2025-09-07 09:01:09 +0100

    Fixing flake8

--- main.py
+++ main.py
@@ -2,11 +2,13 @@ import sys
 
 from my_module.greeting.greetings import Greetings
 
+
 def main():
     greetings = Greetings()
     result = greetings.greet(sys.argv)
     for value in result:
         print(value)
+
 
 if __name__ == '__main__':
     main()
