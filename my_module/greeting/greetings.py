8ad1c7b7da5590ea1bfaa877e74302610b7d9678
harishkannarao <harishkannarao@users.noreply.github.com>
2025-11-05 14:48:30 +0000

    Testing async function

--- my_module/greeting/greetings.py
+++ my_module/greeting/greetings.py
@@ -5,3 +5,7 @@ class Greetings:
         for arg in input_args:
             result.append(f"Hello, {arg}")
         return result
+
+    @staticmethod
+    async def async_greet(input_args) -> list[str]:
+        return Greetings().greet(input_args)
--- tests/greeting/test_greetings.py
+++ tests/greeting/test_greetings.py
@@ -1,3 +1,5 @@
+import asyncio
+
 import pytest
 
 from my_module.greeting.greetings import Greetings
@@ -13,5 +15,13 @@ def test_greetings(setup):
     input_value = ["value1", "value2"]
     under_test = setup
     result = under_test.greet(input_value)
+    assert result[0] == "Hello, value1"
+    assert result[1] == "Hello, value2"
+
+
+def test_async_greetings(setup):
+    input_value = ["value1", "value2"]
+    under_test = setup
+    result: list[str] = asyncio.run(under_test.async_greet(input_value))
     assert result[0] == "Hello, value1"
     assert result[1] == "Hello, value2"
