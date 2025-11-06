2e93bb8501b0db0f7b0ea7e0c5e8275ebc73e51c
harishkannarao <harishkannarao@users.noreply.github.com>
2025-11-06 19:44:45 +0000

    Mocking Class with mocker

--- tests/greeting/test_mocking_function_in_same_file.py
+++ tests/greeting/test_mocking_function_in_same_file.py
@@ -1,6 +1,7 @@
 from unittest.mock import MagicMock
 
 from pytest import MonkeyPatch
+from pytest_mock import MockerFixture
 
 
 class Test:
@@ -14,10 +15,26 @@ def test_add_real() -> None:
     assert result == 4
 
 
-def test_add_mocked(monkeypatch: MonkeyPatch) -> None:
+def test_add_mocked_with_monkey_patch(monkeypatch: MonkeyPatch) -> None:
     mock_add: MagicMock = MagicMock()
     mock_add.side_effect = [3, 5]
     monkeypatch.setattr(Test, "add", mock_add)
+
+    assert Test.add(2, 3) == 3
+    assert Test.add(a=3, b=4) == 5
+
+    calls_made: list = mock_add.call_args_list
+    assert len(calls_made) == 2
+    assert calls_made[0].args[0] == 2
+    assert calls_made[0].args[1] == 3
+    assert calls_made[1].kwargs["a"] == 3
+    assert calls_made[1].kwargs["b"] == 4
+
+
+def test_add_mocked_with_mocker(mocker: MockerFixture) -> None:
+    mock_add: MagicMock = MagicMock()
+    mock_add.side_effect = [3, 5]
+    mocker.patch.object(Test, "add", mock_add)
 
     assert Test.add(2, 3) == 3
     assert Test.add(a=3, b=4) == 5
