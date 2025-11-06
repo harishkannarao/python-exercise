47b2d7e623636ca43aeb590ab16068313d61434c
harishkannarao <harishkannarao@users.noreply.github.com>
2025-11-06 19:34:46 +0000

    Integration tests with mocker

--- tests/service/test_process.py
+++ tests/service/test_process.py
@@ -46,3 +46,25 @@ def test_get_response_mocker_fixture(mocker: MockerFixture) -> None:
     assert len(mock_get_response_from_endpoint.call_args_list) == 1
 
     assert mock_get_response_from_endpoint.call_args.args == (input_url,)
+
+
+def test_get_response_integration_with_mocker(mocker: MockerFixture) -> None:
+    mock_get_response_from_endpoint: MagicMock = MagicMock()
+    mock_response: MagicMock = MagicMock()
+    mocker.patch(
+        "app.external.interface.requests.get",
+        mock_get_response_from_endpoint,
+    )
+
+    expected: str = "test-response"
+    mock_get_response_from_endpoint.return_value = mock_response
+    mock_response.text = expected
+
+    input_url: str = "http://www.example.com"
+    result: str = get_response(input_url)
+
+    assert_that(result).is_equal_to(expected)
+
+    assert len(mock_get_response_from_endpoint.call_args_list) == 1
+
+    assert mock_get_response_from_endpoint.call_args.args == (input_url,)
