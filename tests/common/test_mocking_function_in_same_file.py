bf1950672d8a8c995c4d5ef3b410528ef999e425
harishkannarao <harishkannarao@users.noreply.github.com>
2025-11-06 19:55:56 +0000

    Using only mocker fixture

--- tests/common/test_mocking_function_in_same_file.py
+++ tests/common/test_mocking_function_in_same_file.py
@@ -1,6 +1,5 @@
 from unittest.mock import MagicMock
 
-from pytest import MonkeyPatch
 from pytest_mock import MockerFixture
 
 
@@ -13,23 +12,6 @@ class Test:
 def test_add_real() -> None:
     result: int = Test.add(2, 2)
     assert result == 4
-
-
-def test_add_mocked_with_monkey_patch(monkeypatch: MonkeyPatch) -> None:
-    mock_add: MagicMock = MagicMock()
-    mock_add.side_effect = [3, 5]
-    monkeypatch.setattr(Test, "add", mock_add)
-
-    assert Test.add(2, 3) == 3
-    assert Test.add(a=3, b=4) == 5
-
-    calls_made: list = mock_add.call_args_list
-    assert len(calls_made) == 2
-    assert calls_made[0].args[0] == 2
-    assert calls_made[0].args[1] == 3
-    assert calls_made[1].kwargs["a"] == 3
-    assert calls_made[1].kwargs["b"] == 4
-
 
 def test_add_mocked_with_mocker(mocker: MockerFixture) -> None:
     mock_add: MagicMock = MagicMock()
--- tests/service/test_process.py
+++ tests/service/test_process.py
@@ -1,34 +1,12 @@
 from unittest.mock import MagicMock
-from pytest import MonkeyPatch
+
 from assertpy import assert_that
 from pytest_mock import MockerFixture
-import app.service.process
-import app.external.interface
+
 from app.service.process import get_response
 
 
-def test_get_response_using_monkey_patch(monkeypatch: MonkeyPatch) -> None:
-    mock_get_response_from_endpoint: MagicMock = MagicMock()
-    monkeypatch.setattr(
-        app.service.process,
-        "get_response_from_endpoint",
-        mock_get_response_from_endpoint,
-    )
-
-    expected: str = "test-response"
-    mock_get_response_from_endpoint.return_value = expected
-
-    input_url: str = "http://www.example.com"
-    result: str = get_response(input_url)
-
-    assert_that(result).is_equal_to(expected)
-
-    assert len(mock_get_response_from_endpoint.call_args_list) == 1
-
-    assert mock_get_response_from_endpoint.call_args.args == (input_url,)
-
-
-def test_get_response_mocker_fixture(mocker: MockerFixture) -> None:
+def test_get_response_unit(mocker: MockerFixture) -> None:
     mock_get_response_from_endpoint: MagicMock = MagicMock()
     mocker.patch(
         "app.service.process.get_response_from_endpoint",
@@ -48,7 +26,7 @@ def test_get_response_mocker_fixture(mocker: MockerFixture) -> None:
     assert mock_get_response_from_endpoint.call_args.args == (input_url,)
 
 
-def test_get_response_integration_with_mocker(mocker: MockerFixture) -> None:
+def test_get_response_integration(mocker: MockerFixture) -> None:
     mock_get_response_from_endpoint: MagicMock = MagicMock()
     mock_response: MagicMock = MagicMock()
     mocker.patch(
