5f3b180b8a565e23cecb3dbeafc5aed8f7426c39
harishkannarao <harishkannarao@users.noreply.github.com>
2025-11-07 10:59:06 +0000

    Improvements

--- tests/common/test_mocking_function_in_same_file.py
+++ tests/common/test_mocking_function_in_same_file.py
@@ -3,24 +3,24 @@ from unittest.mock import MagicMock
 from pytest_mock import MockerFixture
 
 
-class Test:
+class Math:
     @staticmethod
     def add(a: int, b: int) -> int:
         return a + b
 
 
 def test_add_real() -> None:
-    result: int = Test.add(2, 2)
+    result: int = Math.add(2, 2)
     assert result == 4
 
 
 def test_add_mocked_with_mocker(mocker: MockerFixture) -> None:
     mock_add: MagicMock = MagicMock()
     mock_add.side_effect = [3, 5]
-    mocker.patch.object(Test, "add", mock_add)
+    mocker.patch.object(Math, "add", mock_add)
 
-    assert Test.add(2, 3) == 3
-    assert Test.add(a=3, b=4) == 5
+    assert Math.add(2, 3) == 3
+    assert Math.add(a=3, b=4) == 5
 
     calls_made: list = mock_add.call_args_list
     assert len(calls_made) == 2
