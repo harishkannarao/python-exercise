0aa909a51bc304a1166c61aaaa65527290709556
harishkannarao <harishkannarao@users.noreply.github.com>
2025-11-08 17:11:24 +0000

    immutable data class

---
+++ tests/common/test_list_assertions.py
@@ -0,0 +1,32 @@
+import dataclasses
+from types import MappingProxyType
+
+from assertpy import assert_that
+
+
+@dataclasses.dataclass(frozen=True)
+class Person:
+    name: str
+    id: int
+    skills: frozenset[str] = frozenset()
+    alias: tuple[str, ...] = ()
+    meta_info: MappingProxyType[str, str] = dataclasses.field(default_factory= lambda: {})
+
+
+def test_list_assert_on_objects():
+    person1: Person = Person(
+        name="Foo",
+        id=1,
+        skills=frozenset(["java", "python"]),
+        alias=(*["fierce_programmer"],),
+        meta_info=MappingProxyType({"graduation": "university"})
+    )
+    person2: Person = Person(
+        name="Bar",
+        id=2
+    )
+    list_of_persons: tuple[Person, ...] = (person1, person2)
+
+    actual1 = list(filter(lambda person: person.id == 1, list_of_persons))
+
+    assert_that(actual1).contains_only(person1)
